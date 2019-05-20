a4acdfae34c3c4f452006978df5cb1433524e714
Admin <admin@Andys-Mac-Pro.local>
2019-05-20 17:50:19 -0400

    Full Automation changes

--- Automate.py
+++ Automate.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import ElementNotVisibleException
-from urllib.error import HTTPError
+# from urllib.error import HTTPError
 from http.client import RemoteDisconnected
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -22,14 +22,14 @@ from pathlib import Path
 
 # chromedriver location
 
-chromedriver = 'C:\\Users\\StudentBridge\\Desktop\\Campus Compare Scrapers\\chromedriver.exe'
+chromedriver = '/Users/admin/Desktop/Web-Scraper/chromedriver'
 driver = webdriver.Chrome(chromedriver)
 
 # use credentials to create a client to interact with the Google Drive API
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name(
-    "C:\\Users\\StudentBridge\\Desktop\\Campus Compare Scrapers\\Automation-scraper\\client_secret.json", scope)
+    "/Users/admin/Desktop/Web-Scraper/client_secret.json", scope)
 client = gspread.authorize(creds)
 
 
@@ -63,18 +63,32 @@ def getFileName(sheet):
 
 # function to remove all college name formatting
 def stripFormat(str):
-    return str.replace(";", "_").replace(":", "_").replace("-", "_").replace("|", "_").replace("'","_").replace(",", "_").replace("?", "_").replace("-", "_").replace("/", "_").replace("\"", "_").replace("(","").replace(")","").replace("@","_").replace("&","_").replace(".","").replace("!","").replace("#","")
+    # print("Strip Format\n")
+    str = str.replace(";", "").replace(":", "").replace("-", " ").replace("|", " ").replace("| ","").replace("'","").replace(",", " ")\
+    .replace("?", " ").replace("/", " ").replace("\"", " ").replace("(","").replace(")","").replace("@"," ")\
+    .replace("&"," ").replace(".","").replace("!","").replace("#","").replace("  ","")\
+    .replace("1","").replace("1 ","").replace("2","").replace("2 ","").replace("3","").replace("3 ","").replace("4","").replace("4 ","")\
+    .replace("5","").replace("5 ","").replace("6","").replace("6 ","").replace("7","").replace("7 ","").replace("8","").replace("8 ","")\
+    .replace("9","").replace("9 ","").replace("0","").replace("0 ","")\
+    .replace("Video","").replace("video","")\
+    .replace("Promo","").replace("promo","")\
+    .replace("Intro","").replace("intro ","")\
+    .replace("trailer","").replace("Trailer","")\
+    .replace("Fall","").replace("fall","").replace("Fall%s","").replace("fall ","")
+
+    return str.lstrip()
 
 
 # Function to get all ipeds data
 def getIpeds(sheet, college_names):
+    print("Getting IPEDs...\n")
     row_count = 1
 
     # goes through every school in the array
     for school in college_names:
         time.sleep(2)
         # searches through every school in ipeds
-        f = open(Path(r'C:/Users/StudentBridge/Desktop/Campus Compare Scrapers/Automation-scraper/')/"ipeds_ids.csv")
+        f = open(Path(r'/Users/admin/Desktop/Web-Scraper')/"ipeds_ids.csv")
         ipeds_data = csv.reader(f)
 
         for ipeds_id in ipeds_data:
@@ -90,13 +104,15 @@ def getIpeds(sheet, college_names):
 # title function that loops through URLS
 def getPytubeTitle(video_URLS, driver, sheet):
     row_count = 1
+    print("Getting Titles...\n")
 
     # gets title for every video in sheet
     for x in video_URLS:
         try:
             yt = YouTube(x)
             title = yt.title
-            title = stripFormat(title)
+            # title = stripFormat(title)
+
         except VideoUnavailable:
             title = "Video Unavailable"
         except KeyError:
@@ -106,6 +122,9 @@ def getPytubeTitle(video_URLS, driver, sheet):
         row_count += 1
 
 
+
+
+ 
 # #
 # #   ONLY USE IF getPytubeTitle() IS NOT WORKING
 # #
@@ -146,6 +165,7 @@ def getPytubeTitle(video_URLS, driver, sheet):
 
 # function to get thumbnails
 def getThumb(video_URLS, file_name, college_names, driver):
+    print("Downloading Thumbnails\n")
     for x in video_URLS:
         driver.get("https://youtubethumbnailimage.com/") # goes to  website for downloading thumbnails
         search = driver.find_element_by_class_name("urlinput")
@@ -156,44 +176,61 @@ def getThumb(video_URLS, file_name, college_names, driver):
         
 
 def renameThumbs(file_name, college_names):
+    print("Renaming Thumbnails\n")
     count = 0
-    dirPath = Path(r'C:/Users/StudentBridge/Downloads')
-    os.chdir(dirPath)
-    entries = ((os.stat(files), files) for files in os.listdir(dirPath))
-    entries = ((stat[ST_CTIME], files) for stat, files in entries)
-    for cdate, files in sorted(entries):
-        src = dirPath / files
-        dst = dirPath / (file_name[count]+ "___" + college_names[count]  + ".jpg")
-        os.rename(src, dst)
-        count += 1
+    try:
+        dirPath = Path(r'/Users/admin/Downloads')
+        os.chdir(dirPath)
+        entries = ((os.stat(files), files) for files in os.listdir(dirPath))
+        entries = ((stat[ST_CTIME], files) for stat, files in entries)
+        for cdate, files in sorted(entries):
+            src = dirPath / files
+            dst = dirPath / ('/Users/admin/Downloads/Thumbnails' + file_name[count]+ "___" + college_names[count]  + ".jpg")
+            os.rename(src, dst)
+            count += 1
+        os.mkdir('/Users/admin/Downloads/Thumbnails')
+    except IndexError:
+            pass    
+
+
+
+def transferthumbnails(thumbnails,driver, sheet):
+    print("Hello")
 
 # Function to download youtube videos
 def downloadVideos(videos_URLS,file_name, college_names):
+    print("Downloading Videos\n")
     count = 0
     # print("test1")
     for url in videos_URLS:
-        print(url)
-        try:
+        # print(url)
+        try: 
             yt = YouTube(url)
-            video_name = yt.streams.first().download('C:/Users/StudentBridge/Downloads/Videos')
-            print("download test")
-            print(yt.streams.first().default_filename)
-            os.rename(video_name, 'C:/Users/StudentBridge/Downloads/Videos/' + college_names[count] +"___" + file_name[count]+".mp4")
+            video_name = yt.streams.first().download('/Users/admin/Downloads/Videos')
+            # new_name = yt.title +" "+ str(count)
+            # print("download test")
+            # print(yt.streams.first().default_filename)
+            # new_name = "Hello" + "rename" + str(count) + ".mp4"
+            # print(new_name)
+            os.rename(video_name, '/Users/admin/Downloads/Videos/' + college_names[count] +"___" + file_name[count] + ".mp4")
+            # os.rename(video_name, college_names[count] +"___" + file_name[count]+ count + ".mp4")
+            print("renamed")
             count +=1
-            time.sleep(10)
+            time.sleep(3)
+        
         except:
-            print('Error')
-            pass
-        # except KeyError:
+              print("Error")
+         
+        # except: KeyError:
         #     print('KeyError')
         #     pass
-        # except HTTPError:
+        # except: HTTPError:
         #     print('HTTPError')
         #     pass
-        # except RemoteDisconnected:
+        # except: RemoteDisconnected:
         #     print('RemoteError')
         #     pass
-        # except VideoUnavailable:
+        # except: VideoUnavailable:
         #     print('VideoDNEError')
         #     pass
             
@@ -201,8 +238,9 @@ def downloadVideos(videos_URLS,file_name, college_names):
 
 # function to transcode url
 def transcodeVideos(driver, video_titles):
+    print("Transcoding Videos\n")
     wait = WebDriverWait(driver, 200)
-    downloaded_Videos = os.listdir(Path('C:/Users/StudentBridge/Downloads/Videos/'))
+    downloaded_Videos = os.listdir(Path('/Users/admin/Downloads/Videos/'))
 
     # log in to transcoder
     driver.get("https://transcoder.studentbridge.com/admin/")
@@ -231,8 +269,8 @@ def transcodeVideos(driver, video_titles):
 
         # upload video file
         file_upload = driver.find_element_by_id('file_upload')
-        file_upload.send_keys("C:\\Users\\StudentBridge\\Downloads\\Videos\\" + video)
-        print(video)
+        file_upload.send_keys("/Users/admin/Downloads/Videos/" + video)
+        # print(video)
         # select campus compare as encoder profile
         attempts = 0
         max_attempts = 5
@@ -258,6 +296,7 @@ def transcodeVideos(driver, video_titles):
         count += 1
 
 def transferURLS(video_titles, college_names, driver, sheet):
+    print("Tranferring Videos urls....\n")
     # log in to transcoder
     driver.get("https://transcoder.studentbridge.com/admin/")
     email = driver.find_element_by_id("email")
@@ -287,7 +326,7 @@ def transferURLS(video_titles, college_names, driver, sheet):
             print(college_name)
             print(title)
             # search.send_keys(college_name+" "+title)
-            search.send_keys(title)
+            search.send_keys(title +" "+ count)
             # search.send_keys(college_names[count] +"_" + title)
             search.send_keys(Keys.RETURN)
             # print(title)
@@ -317,7 +356,7 @@ def transferURLS(video_titles, college_names, driver, sheet):
             
 
 def deleteVideos():
-    folder = Path("Downloads/Videos")
+    folder = Path("/Users/admin/Downloads/Videos")
     for the_file in os.listdir(folder):
         file_path = os.path.join(folder, the_file)
         try:
@@ -329,6 +368,8 @@ def deleteVideos():
 
 # Main Function
 def automate(sheetname):
+    print("Active\n")
+
     # saves spreadsheet being used
     sheet = client.open(sheetname).sheet1
 
@@ -336,11 +377,6 @@ def automate(sheetname):
     video_URLS = getVideoURLS(sheet)
     college_names = getCollegeNames(sheet)
 
-    # gets ipeds data
-    #getIpeds(sheet, college_names)
-
-    # gets titles for videos
-    #getPytubeTitle(video_URLS, driver, sheet)
 
     # creates array of video titles
     video_titles = getVideoTitles(sheet)
@@ -348,17 +384,22 @@ def automate(sheetname):
     # creates array for file name
     file_name = getFileName(sheet)
 
-    # downloads thumbnails from youtube
-    # getThumb(video_URLS, file_name, college_names, driver)
+     # gets ipeds data
+    # getIpeds(sheet, college_names)
 
-    time.sleep(10)
+    # gets titles for videos
+    # getPytubeTitle(video_URLS, driver, sheet)
+
+    # downloads thumbnails from youtube
+    getThumb(video_URLS, file_name, college_names, driver)
+
+    # time.sleep(10)
 
     # renames downloaded thumbnails
-    # renameThumbs(file_name, college_names)
+    renameThumbs(file_name, college_names)
 
     # downloads all videos and saves them in "College Name___ Video Title"
-    downloadVideos(video_URLS,file_name, college_names)
-
+    # downloadVideos(video_URLS,file_name, college_names)
     # transcodes every video
     # transcodeVideos(driver, video_titles)
 
@@ -367,4 +408,4 @@ def automate(sheetname):
     # transferURLS(video_titles,college_names,driver,sheet);
     
 
-automate("Maryland_Videos_Import_Format")
+automate("Mississippi_Videos_Import_Format")
